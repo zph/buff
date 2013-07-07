bc015e6cb97630b957e7c1aea0729ece312352e4
ZPH <Zander@civet.ws>
2013-07-07 16:56:46 -0700

    Removed commented text

--- spec/lib/buff/encode_spec.rb
+++ spec/lib/buff/encode_spec.rb
@@ -1,35 +1,5 @@
 require "spec_helper"
 
-  # describe "Addressable Explorations" do
-
-  #   describe "addressable conversions" do
-
-  #     let(:uri) { Addressable::URI.new }
-
-  #     it "should convert {:pony => 'pony' }" do
-  #       h = { pony: "pony"}
-  #       uri.query_values = h
-  #       uri.query.should eq("pony=pony")
-  #     end
-
-  #     it "should convert {:pony => ['pony', 'horse'] }" do
-  #       h = { pony: ["pony", "horse"]}
-  #       uri.query_values = h
-  #       uri.query.should eq("pony=pony&pony=horse")
-  #     end
-
-  #     it "should convert {:days => [mon, tues], :times => [....]}" do
-  #       h = { days: ["mon", "tue"], times: ["12:00", "13:00"]}
-  #       uri.query_values = h
-  #       uri.query.should eq("days=mon&days=tue&times=12%3A00&times=13%3A00")
-  #     end
-  #     xit "should convert {schedules => {:days => [mon, tues], :times => [....]}" do
-  #       h = { schedules: { :days => ["mon", "tue", "wed", "thu"], :times => ["12:00", "13:00"]} }
-  #       # h = {:a => "a", :bd => ["c", "d", "e"]}
-  #       uri.query_values = h
-  #       uri.query.should eq("days=mon&days=tue&times=12%3A00&times=13%3A00")
-  #     end
-  #   end
 describe Buff::Encode do
 
   context "successful code" do
